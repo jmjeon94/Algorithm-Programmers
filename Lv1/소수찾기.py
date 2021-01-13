5cdb1d520c34e63473aac20b577389ff9388dc92
jmjeon94 <jmjeon3155@gmail.com>
2021-01-13 10:39:10 +0900

    Lv1 나누어 떨어지는 숫자 배열(수정)

--- Lv1/소수찾기.py
+++ Lv1/소수찾기.py
@@ -1,7 +1,14 @@
 def solution(n):
+    answer = [1] * (n+1)
+    for i in range(2, int(n**(1/2))+1):
+        j = 2
+        while j*i <= n:
+            answer[j*i] = 0
+            j+=1
 
-    for i in range(int(n**(1/2))):
+    del answer[0]
+    return sum(answer)-1 # 1은 제외
 
-    return answer
+print(solution(5))
 
 
