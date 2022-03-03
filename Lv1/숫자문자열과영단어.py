9a2cc74ab4cfadbf6b50702fbf075703a6aa976f
jmjeon94 <jmjeon3155@gmail.com>
2022-03-03 15:11:20 +0900

    Lv1 숫자 문자열과 영단어

---
+++ Lv1/숫자문자열과영단어.py
@@ -0,0 +1,10 @@
+def solution(s):
+
+    nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i, n in enumerate(nums):
+        s = s.replace(n, str(i))
+    return int(s)
+
+
+print(solution('2three45sixseven'))
