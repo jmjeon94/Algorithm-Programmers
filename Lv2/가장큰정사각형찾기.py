b4d19ce640d2158c7a3c92f6166de50b482d08a9
jmjeon94 <jmjeon3155@gmail.com>
2022-03-08 11:02:55 +0900

    Lv2 가장 큰 정사각형 찾기

--- Lv2/가장큰정사각형찾기.py
+++ Lv2/가장큰정사각형찾기.py
@@ -1,8 +1,13 @@
 def solution(board):
-    answer = 1234
+    answer = 0
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            if board[i][j]==1:
+                board[i][j] = min([board[i-1][j], board[i][j-1], board[i-1][j-1]]) + 1
+
+    for i in range(len(board)):
+        answer = max(answer, max(board[i]))
+    return answer**2
 
 
-    return answer
-
-
-print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
+print(solution([[0,0],[0,0]]))
