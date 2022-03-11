93c01ff716335e64dba00b7aab3f6eef39c73b71
jmjeon94 <jmjeon3155@gmail.com>
2022-03-11 11:26:48 +0900

    Lv2 거리두기 확인하기

---
+++ Lv2/거리두기확인하기.py
@@ -0,0 +1,36 @@
+answer = [1] * 5
+
+def dfs(place, i, j, depth, visited, pth):
+
+    visited[i][j] = 1
+
+    if place[i][j]=='P' and depth>0:
+        answer[pth] = 0
+        return
+
+    if depth>1:
+        return
+
+    for y, x in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+        if i+y>4 or i+y<0 or j+x>4 or j+x<0:
+            continue
+        if not place[i+y][j+x]=='X' and not visited[i+y][j+x]:
+            dfs(place, i+y, j+x, depth+1, visited, pth)
+
+    visited[i][j] = 0
+
+def solution(places):
+    n = 0
+    for place in places:
+        for i in range(len(place)):
+            for j in range(len(place[0])):
+                visited = [[0] * 5 for _ in range(5)]
+                if place[i][j] == 'P':
+                    dfs(place, i, j, 0, visited, n)
+        n+=1
+    return answer
+
+
+print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+                ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+                ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
