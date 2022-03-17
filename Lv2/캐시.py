f32a01d78223aba25a8106489e566a46c9f7790d
jmjeon94 <jmjeon3155@gmail.com>
2022-03-18 01:00:28 +0900

    Lv2 캐시

--- Lv2/캐시.py
+++ Lv2/캐시.py
@@ -1,3 +1,21 @@
 def solution(cacheSize, cities):
     answer = 0
+
+    cache = []
+    for city in cities:
+        city = city.lower()
+        if not city in cache:
+            cache.append(city)
+            if len(cache) > cacheSize:
+                cache.pop(0)
+            answer += 5
+
+        else:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+
     return answer
+
+
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
